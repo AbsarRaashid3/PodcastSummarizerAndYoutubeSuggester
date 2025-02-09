e804b2dc56a9ecc94462b405027665df13faf975
AbsarRaashid <97013327+AbsarRaashid3@users.noreply.github.com>
2025-02-09 21:46:35 +0500

    making it streamlit ready

--- PodcastSummarizerAndYoutubeSuggester.py
+++ PodcastSummarizerAndYoutubeSuggester.py
@@ -9,16 +9,16 @@ from googleapiclient.discovery import build
 import re
 import requests
 import subprocess
+import streamlit as st
 
+API_KEY = st.secrets["API_KEY"]
+DEEPGRAM_API_KEY= st.secrets["DEEPGRAM_API_KEY"]
 # Set environment variables
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 # Load NLP model
 nlp = spacy.load("en_core_web_sm")
 
-# Set API Key (Replace with your YouTube API Key)
-API_KEY = "AIzaSyAWQ-Q9PJxOwXirog5-3zV9_PvakwCKxh8"
-DEEPGRAM_API_KEY = "6672021d942acd8ec7df300c71a6b9b56abf4d0b" # Replace with your actual key
 
 # Initialize session state
 if "summary" not in st.session_state:
