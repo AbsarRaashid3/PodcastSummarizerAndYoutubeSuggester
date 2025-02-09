2bfa084d3fa185edb94c2774dcded77af64019d0
AbsarRaashid <97013327+AbsarRaashid3@users.noreply.github.com>
2025-02-09 21:54:01 +0500

    Update PodcastSummarizerAndYoutubeSuggester.py

--- PodcastSummarizerAndYoutubeSuggester.py
+++ PodcastSummarizerAndYoutubeSuggester.py
@@ -16,12 +16,7 @@ DEEPGRAM_API_KEY= st.secrets["DEEPGRAM_API_KEY"]
 # Set environment variables
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-
-model_name = "en_core_web_sm"
-if not spacy.util.is_package(model_name):
-    os.system(f"python -m spacy download {model_name}")
-
-nlp = spacy.load(model_name)
+nlp = spacy.load("en_core_web_sm")
 
 # Initialize session state
 if "summary" not in st.session_state:
